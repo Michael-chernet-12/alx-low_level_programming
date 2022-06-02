83dda956d9848cb396257a66f953d843c15cf0ae
Michael chernet <zemichaelchernet@gmail.com>
2022-06-02 15:08:16 -0700

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,27 @@
-#!/bin/bash
- ‘=’, ‘,’, ‘;’, ‘asm’ or ‘__attribute__’ before ‘-’ token
+#include <stdio.h>
 
-     2 | gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o quote 101-quote.c
+/**
+ *
+ *  * main - main function
+ *
+ *   *
+ *
+ *    * Return: end program
+ *
+ *     */
 
-           |     ^
+int main(void)
+
+{
+
+		char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+
+
+			fprintf(stderr, "%s\n", n);
+
+
+
+				return (1);
+
+}
