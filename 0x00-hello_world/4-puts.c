70f53c7822e3875a68f269989f6ba4ba85260cae
Michael chernet <zemichaelchernet@gmail.com>
2022-06-02 12:31:58 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,33 @@
-#!/bin/bash
-gcc -o cisfun $CFILE
+/*
+ *
+ *  *File: 4-puts.c
+ *
+ *  */
+
+#include<stdio.h>
+
+
+
+/**
+ *
+ *  * main - Prints "Programming is like building a multilingual
+ *
+ *   *                puzzle,followed by a new line.
+ *
+ *    *
+ *
+ *     *Return: always 0.
+ *
+ *      */
+
+int main(void)
+
+{
+
+	  puts("\"Programming is like building a multilingual puzzle");
+
+
+
+	    return (0);
+
+}
