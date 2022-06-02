51fa0ea6daee4b0b17b37be55e9e61ae4283e626
Michael chernet <zemichaelchernet@gmail.com>
2022-06-02 16:30:30 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
-
 /**
+ * main - main function
  *
- *  * main - main function
- *
- *   *
- *
- *    * Return: end program
- *
- *     */
-
+ * Return: end program
+ */
 int main(void)
-
 {
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-		char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fprintf(stderr, "%s\n", n);
 
-
-
-			fprintf(stderr, "%s\n", n);
-
-
-
-				return (1);
-
+	return (1);
 }
