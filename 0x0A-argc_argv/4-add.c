b3fa2cc81b4c5dc3d4444fd4d2a942fb10020714
Michael chernet <zemichaelchernet@gmail.com>
2022-06-24 07:26:59 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,50 +1,35 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
+
 /**
- * isNumeric - checks if string  is numeric
- *
- * @s: string to check
- *
- * Return: true if is numeric, otherwise return false
- */
-bool isNumeric(char *s)
-{
-	int i;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] < 48 || s[i] > 57)
-		{
-			return (false);
-		}
-	}
-	return (true);
-}
-/**
- * main - Entry Point
- *
- * @argc: arg counter
- * @argv: arguments array
- *
- * Return: Always 0 (Success)
+ * main -  print name
+ * @argc: int.
+ * @argv: char.
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int sum = 0;
 	int i;
-	(void)argc;
-	(void)argv;
+	int j;
+	int sum = 0;
 
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!isNumeric(argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		sum += atoi(argv[i]);
+		sum = sum + atoi(argv[i]);
 	}
 	printf("%d\n", sum);
-
 	return (0);
 }
