dbcde01ace75b6b90dc95aff890e0a2154f8555d
Michael chernet <zemichaelchernet@gmail.com>
2022-06-24 07:12:37 -0700

    2 args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - Entry Point
- *
- * @argc: arg counter
- * @argv: arguments array
- *
- * Return: Always 0 (Success)
+ *  main -  print name
+ *  @argc: int.
+ *  @argv: char.
+ *  Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
-	(void)argc;
-	(void)argv;
 
-	if (argc < 3)
+	int i;
+
+	for (i = 0 ; i < argc ; i++)
 	{
-		printf("Error\n");
-		return (1);
+		printf("%s\n", argv[i]);
 	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-
-	printf("%d\n", a * b);
-
 	return (0);
 }
